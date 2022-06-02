344a2bbd9a98a4493842843041fb94bc7afca152
shiliangL <1462999406@qq.com>
2022-06-02 14:01:46 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<!--
+ * @Author: shiliangL
+ * @Date: 2022-06-02 13:46:48
+ * @LastEditTime: 2022-06-02 13:51:09
+ * @LastEditors: Do not edit
+ * @Description: 
+-->
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+xxxxxxx
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + Vite" />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
