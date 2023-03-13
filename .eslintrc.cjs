9bcea9df0ae93b7394bdd2859eac536f52574c12
shiliangL <1462999406@qq.com>
2023-03-13 23:04:47 +0800

    change opt and init

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'standard'
+  ],
+  overrides: [],
+  globals: {
+
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'vue'
+  ],
+  rules: {}
+}
