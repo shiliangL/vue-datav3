0038b3cb60a12766a650a5089aa7bddb210a8a7f
shiliangL <1462999406@qq.com>
2023-03-14 09:58:13 +0800

    2023年3月14日++

---
+++ src/datav/coreChart/index.vue
@@ -0,0 +1,18 @@
+
+<script>
+import { h, defineComponent } from '@vue/composition-api'
+export default defineComponent({
+  setup () {
+    return () => h('div', {
+      class: 'datav_core_chart'
+    })
+  }
+})
+</script>
+
+<style scoped>
+.datav_core_chartP {
+  width: 100%;
+  height: 100%;
+}
+</style>
