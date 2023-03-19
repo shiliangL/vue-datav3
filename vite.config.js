fa61603f71273cbb2e7fc4bacf498625fc951fb6
shiliangL <1462999406@qq.com>
2023-03-19 17:26:08 +0800

    2023年3月18日++

--- vite.config.js
+++ vite.config.js
@@ -30,8 +30,8 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        nested: resolve(__dirname, 'nested/index.html')
+        main: resolve(__dirname, 'index.html')
+        // nested: resolve(__dirname, 'nested/index.html')
       },
       output: {
         manualChunks: {
