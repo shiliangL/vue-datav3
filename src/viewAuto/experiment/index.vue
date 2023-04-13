077aeaa168b2c8f225353c3a97895c2ba3a970c4
shiliangL <1462999406@qq.com>
2023-04-13 22:08:18 +0800

    2023年4月13日

--- src/viewAuto/experiment/index.vue
+++ src/viewAuto/experiment/index.vue
@@ -1,32 +1,23 @@
 <template>
   <div class="experiment">
-
     <div class="datav_card_list">
       <div class="datav_card_list_01"></div>
       <div class="datav_card_list_02"></div>
       <div class="datav_card_list_03"></div>
-      <div
-        class="datav_card_list_04"
-        style="--color_line:#3C91FA"
-      ></div>
-      <div
-        class="datav_card_list_04"
-        style="--color_line:#F9AA49"
-      ></div>
-      <div
-        class="datav_card_list_04"
-        style="--color_line:#63AE3B"
-      ></div>
-      <div
-        class="datav_card_list_04"
-        style="--color_line:#DD555D"
-      ></div>
+      <div class="datav_card_list_04" style="--color_line: #3c91fa"></div>
+      <div class="datav_card_list_04" style="--color_line: #f9aa49"></div>
+      <div class="datav_card_list_04" style="--color_line: #63ae3b"></div>
+      <div class="datav_card_list_04" style="--color_line: #dd555d"></div>
+
+      <div class="datav_card_list_044">
+        <div class="after"></div>
+        datav_card_list_044
+      </div>
+
+      <div class="datav_card_list_05"></div>
     </div>
 
-    <div
-      class="RecycleScroller"
-      v-if="0"
-    >
+    <div class="RecycleScroller" v-if="0">
       <recycleScroller
         class="scroller"
         :items="list"
@@ -35,30 +26,24 @@
         v-slot="{ item }"
         ref="recycleScrollerRef"
       >
-        <div class="user">
-          {{ item.name }} {{ item.id }}
-        </div>
+        <div class="user">{{ item.name }} {{ item.id }}</div>
       </recycleScroller>
     </div>
 
     <div class="grid">
-      <gridPanel
-        :column="2"
-        :gap="10"
-      >
+      <gridPanel :column="2" :gap="10">
         <!-- 'gridColumn': '1/3',  'gridRow': '1 / 2'} -->
-        <div :style="{background:list[1]}"> 111 </div>
-        <div :style="{background:list[2]}"> 222 </div>
-        <div :style="{background:list[0], 'gridColumn': '1/3'}"> 000 </div>
-        <div :style="{background:list[3]}"> 333 </div>
-        <div :style="{background:list[4]}"> 333 </div>
+        <div :style="{ background: list[1] }">111</div>
+        <div :style="{ background: list[2] }">222</div>
+        <div :style="{ background: list[0], gridColumn: '1/3' }">000</div>
+        <div :style="{ background: list[3] }">333</div>
+        <div :style="{ background: list[4] }">333</div>
       </gridPanel>
     </div>
   </div>
 </template>
 
 <script>
-
 import 'vue-virtual-scroller/dist/vue-virtual-scroller.css'
 import { RecycleScroller } from 'vue-virtual-scroller'
 import { randomDarkRgbColor } from '@/chartOpt/index'
@@ -69,7 +54,7 @@ export default defineComponent({
     RecycleScroller
   },
   setup () {
-    const list = new Array(10).fill(0).map(item => randomDarkRgbColor())
+    const list = new Array(10).fill(0).map((item) => randomDarkRgbColor())
     console.log(list, '=list=')
     return {
       list
@@ -155,6 +140,101 @@ export default defineComponent({
     height: 100%;
     position: absolute;
     background: var(--color_line);
+  }
+}
+
+.datav_card_list_044 {
+  --color_line: #3c91fa;
+  width: 480px;
+  height: 40px;
+  display: flex;
+  align-items: center;
+  overflow: hidden;
+  position: relative;
+  background: rgba(55, 83, 112, 0.35);
+  background-image: linear-gradient(
+    135deg,
+    rgb(77, 204, 253) 0%,
+    rgb(69, 117, 237) 75%
+  );
+  .after {
+    left: 0;
+    top: 0;
+    height: 70px;
+    width: 200px;
+    position: absolute;
+    transform: rotate(310deg);
+    transform-origin: 12px 50px;
+    backdrop-filter: blur(1px);
+    background-color: rgba(255, 255, 255, 0.25);
+    border-radius: 12px;
+    color: rgba(255, 255, 255, 0.75);
+    box-shadow: rgba(0, 0, 0, 0.12) 0px 6px 26px, rgba(0, 0, 0, 0.04) 0px 0px 6px;
+  }
+  &::after {
+    content: "";
+    right: 0;
+    bottom: 0;
+    height: 10px;
+    width: 10px;
+    position: absolute;
+    background: #56799d;
+    clip-path: polygon(51% 100%, 100% 50%, 100% 100%);
+  }
+  &::before {
+    z-index: 2;
+    content: "";
+    left: 0;
+    width: 2px;
+    height: 100%;
+    position: absolute;
+    background: var(--color_line);
+  }
+}
+
+.datav_card_list_05 {
+  --borderWidth: 1px;
+
+  width: 328px;
+  height: 88px;
+  background: #000;
+  position: relative;
+  border-radius: var(--borderWidth);
+
+  &:after {
+    content: "";
+    position: absolute;
+    top: calc(-1 * var(--borderWidth));
+    left: calc(-1 * var(--borderWidth));
+    height: calc(100% + var(--borderWidth) * 2);
+    width: calc(100% + var(--borderWidth) * 2);
+    background: linear-gradient(
+      60deg,
+      #f79533,
+      #f37055,
+      #ef4e7b,
+      #a166ab,
+      #5073b8,
+      #1098ad,
+      #07b39b,
+      #6fba82
+    );
+    z-index: -1;
+    border-radius: calc(2 * var(--borderWidth));
+    animation: animatedgradient 3s ease alternate infinite;
+    background-size: 300% 300%;
+  }
+
+  @keyframes animatedgradient {
+    0% {
+      background-position: 0% 50%;
+    }
+    50% {
+      background-position: 100% 50%;
+    }
+    100% {
+      background-position: 0% 50%;
+    }
   }
 }
 </style>
